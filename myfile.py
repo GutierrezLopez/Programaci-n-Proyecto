1880803cbfeb9b4d5d59df569f1580f61ec74826
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-19 15:04:30 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,7 +5,7 @@ import numpy as np
 url= "https://files.minsa.gob.pe/s/t9AFqRbXw3F55Ho/download"
 
 st.title("Proyecto Programación 2022-2")
-st.write("HOLA CONS")
+st.write("HOLA **CONS")
 st.line_chart()
 
 st.title()
