f76bd809804c8301c2d4b48ad9f72219d3e15251
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-19 15:02:04 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+url= "https://files.minsa.gob.pe/s/t9AFqRbXw3F55Ho/download"
+
 st.title("Proyecto Programación 2022-2")
-st.write()
+st.write("HOLA CONS")
 st.line_chart()
 
 st.title()
