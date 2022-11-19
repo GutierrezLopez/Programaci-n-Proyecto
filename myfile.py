7bd5d7a6c428e37a38de143874d728580d03c9bd
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-19 12:38:07 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,17 +1,17 @@
 import streamlit as st
-import pandas  as pd
-import numpy as  np
+import pandas as pd
+import numpy as np
 
-st . title ( "Proyecto Programación 2022-2" )
-st . escribir ()
-st . gráfico_de_líneas ()
+st.title("Proyecto Programación 2022-2")
+st.write()
+st.line_chart()
 
-st . título ()
-st . escribir ()
-st . gráfico_de_líneas ()
+st.title()
+st.write()
+st.line_chart()
 
-st . título ()
-x = PD . marco de datos (
-  np _ al azar randn ( 1000 , 2 ) / [ 50 , 50 ] + []
-  columnas = [])
-st . mapa ( x )
+st.title()
+x=pd.DataFrame(
+  np.random.randn(1000,2)/[50,50]+[]
+  columns=[])
+st.map(x)
