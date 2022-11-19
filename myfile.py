40abd29cf25c65af6df1def2c00bb436daac7f5c
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-19 12:32:53 -0500

    Create myfile.py

---
+++ myfile.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas  as pd
+import numpy as  np
+
+st . title ( "Proyecto Programación 2022-2" )
+st . escribir ()
+st . gráfico_de_líneas ()
+
+st . título ()
+st . escribir ()
+st . gráfico_de_líneas ()
+
+st . título ()
+x = PD . marco de datos (
+  np _ al azar randn ( 1000 , 2 ) / [ 50 , 50 ] + []
+  columnas = [])
+st . mapa ( x )
