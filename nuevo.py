d1d8f2c34f2f751a9d662d0988808c56c6638046
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 13:15:26 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,4 +1,10 @@
-python -m pip install -U matplotlib --prefer-binary
+python -m pip install \
+  --upgrade \
+  --pre \
+  --index-url https://pypi.anaconda.org/scipy-wheels-nightly/simple \
+  --extra-index-url https://pypi.org/simple \
+  matplotlib
+    
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.bar([1, 2, 3], [3, 2, 1])
