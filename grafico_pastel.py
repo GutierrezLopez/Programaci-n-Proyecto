2b965ee0e1a1ffe375b99d842148716b0230d6d6
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 12:16:49 -0500

    Update and rename grafico_pastel.php to grafico_pastel.py

---
+++ grafico_pastel.py
@@ -0,0 +1,7 @@
+ import matplotlib.pyplot as plt 
+ porcentaje = [63.27,36.73]
+    sexo = ["M","F"]
+    colores = ["#60D394","#FFD97D"]
+    plt.pie(porcentaje, labels=sexo, autopct="%0.1f %%", colors=colores)
+    plt.axis("equal")
+    plt.show()
