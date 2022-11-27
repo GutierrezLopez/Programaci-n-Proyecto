06aa9f31e4d3188f04241751cb6704d5d880700c
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 13:18:56 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,9 +1,4 @@
-python -m pip install \
-  --upgrade \
-  --pre \
-  --index-url https://pypi.anaconda.org/scipy-wheels-nightly/simple \
-  --extra-index-url https://pypi.org/simple \
-  matplotlib
+#$ python -m pip install 
     
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
