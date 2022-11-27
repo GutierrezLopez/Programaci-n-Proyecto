052a438872e549ad55a19c176aea1ee657662e95
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 12:26:23 -0500

    Update nuevo.py

--- nuevo.py
+++ nuevo.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-porcentaje = [63.27,36.73]
+porcentaje = [64,36]
 sexo = ["M","F"]
 colores = ["#60D394","#FFD97D"]
 plt.pie(porcentaje, labels=sexo, autopct="%0.1f %%", colors=colores)}
 plt.axis("equal")
 plt.show()
+
